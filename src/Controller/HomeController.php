7636a2744e76dbeedbb7f6860f55faf73987be58
Kai-Joris <paulkassel1919@gmail.com>
2023-09-06 12:04:05 +0200

    Keine Ahnung

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
 {
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        var_dump($request);
         return new Response();
     }
 
